36f9b086223a8ab662c46dab81d95036f84bdaf3
reddisunitha <48647541+reddisunitha@users.noreply.github.com>
2024-10-02 14:38:24 +0530

    Update EmployeeService.java

--- src/main/java/org/cg/service/EmployeeService.java
+++ src/main/java/org/cg/service/EmployeeService.java
@@ -1,51 +1,50 @@
 package org.cg.service;
 
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
 
-import org.cg.model.Address;
-import org.cg.model.Employee;
-import org.cg.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.example.employee.model.Employee;
+import com.example.employee.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
-	@Autowired
-	private EmployeeRepository employeeRepository;
 
-	public List<Employee> findAll() {
-		List<Employee> emps = (List<Employee>) employeeRepository.findAll();
-		return emps;
-	}
-	public Employee getEmployee(long id){
-		return employeeRepository.findOne(id);
-	}
-	public void addEmployee(Employee e) {
-		for(Address address : e.getAddress()) {
-			address.setEmployee(e);
-		}
-		employeeRepository.save(e);
-	}
-	public void updateEmployee(Employee emp, int id){
-		if(id == emp.getEmployeeId()) {
-			employeeRepository.save(emp);
-		}
-	}
-	public void deleteAllEmployees(){
-		employeeRepository.deleteAll();
-	}
-	public void deleteEmployeeByID(long id){
-		employeeRepository.deleteById(id);
-	}
-	public void patchEmployee(Employee emp, int id) {
-		if(id == emp.getEmployeeId()) {
-			employeeRepository.save(emp);
-		}
-	}
-	
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Map<String, Object> calculateTaxDeductions(String employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow();
+        double yearlySalary = employee.getSalary() * (12 - calculateMonthsUntilNow(employee.getDoj()));
+        
+        double taxAmount = calculateTax(yearlySalary);
+        double cessAmount = (yearlySalary > 2500000) ? (yearlySalary - 2500000) * 0.02 : 0;
+
+        return Map.of(
+                "employeeId", employee.getEmployeeId(),
+                "firstName", employee.getFirstName(),
+                "lastName", employee.getLastName(),
+                "yearlySalary", yearlySalary,
+                "taxAmount", taxAmount,
+                "cessAmount", cessAmount
+        );
+    }
+
+    private long calculateMonthsUntilNow(LocalDate doj) {
+        return ChronoUnit.MONTHS.between(doj, LocalDate.now());
+    }
+
+    private double calculateTax(double salary) {
+        if (salary <= 250000) return 0;
+        else if (salary <= 500000) return (salary - 250000) * 0.05;
+        else if (salary <= 1000000) return 12500 + (salary - 500000) * 0.1;
+        else return 37500 + (salary - 1000000) * 0.2;
+    }
 }
