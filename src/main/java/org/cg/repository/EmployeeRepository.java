a60fc10a91824fb567b954b45658b192c3deea8e
reddisunitha <48647541+reddisunitha@users.noreply.github.com>
2024-10-02 14:37:12 +0530

    Update EmployeeRepository.java

--- src/main/java/org/cg/repository/EmployeeRepository.java
+++ src/main/java/org/cg/repository/EmployeeRepository.java
@@ -1,10 +1,9 @@
 package org.cg.repository;
 
-import org.cg.model.Employee;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+import com.example.employee.model.Employee;
 
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
+    boolean existsByEmployeeId(String employeeId);
 }
