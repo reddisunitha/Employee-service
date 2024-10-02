c44c65d4cf185b4910cc06c220bc8adb6ef3c19f
reddisunitha <48647541+reddisunitha@users.noreply.github.com>
2024-10-02 16:51:39 +0530

    Create GlobalExceptionHandler.java

---
+++ src/main/java/org/cg/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package org.cg.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
